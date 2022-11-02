c0babe5b7b2e58e165c75b87f30d1b6c717ae09f
Emanuele213 <vittorius213@gmail.com>
2022-11-02 16:39:13 +0100

    add number on for

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-const show = document.querySelector('.main-container');
+const eleGrid = document.querySelector('.main-container');
+
 
 for (let i = 1; i <= 100; i++) {
-    const element = `<div class="sq">${i}</div>`;
-        show.innerHTML += element;
-};
+	const eleSq = document.createElement('div');
+	eleSq.classList.add('sq');
+	eleGrid.append(eleSq);
+    eleSq.append([i]);
+
+	eleSq.addEventListener('click', function () {
+		this.classList.toggle('overlay')
+	});
+}
+
