dd6786c514c26033241f9b41be4627cd52ed3ba2
Emanuele213 <vittorius213@gmail.com>
2022-11-02 18:15:34 +0100

    bonus completed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,44 @@
 const eleGrid = document.querySelector('.main-container');
+const eleSend = document.querySelector('.mod-btn'); 
+const difficulty = document.querySelector('#difficolta');
 
+eleSend.addEventListener('click', function () { 
+    const eleDifficulty = difficulty.value;
+    eleGrid.replaceChildren();
+if (eleDifficulty == 'easy') {
+    for (let i = 1; i <= 100; i++) {
+        const eleSq = document.createElement('div');
+        eleSq.classList.add('sq');
+        eleGrid.append(eleSq);
+        eleSq.append([i]);
 
-for (let i = 1; i <= 100; i++) {
-	const eleSq = document.createElement('div');
-	eleSq.classList.add('sq');
-	eleGrid.append(eleSq);
-    eleSq.append([i]);
+        eleSq.addEventListener('click', function () {
+            this.classList.toggle('overlay')
+        });
+    }
+}else if (eleDifficulty == 'hard') {
+    for (let i = 1; i <= 81; i++) {
+        const eleSq = document.createElement('div');
+        eleSq.classList.add('sq', 'small');
+        eleGrid.append(eleSq);
+        eleSq.append([i]);
+        
 
-	eleSq.addEventListener('click', function () {
-		this.classList.toggle('overlay')
-	});
+        eleSq.addEventListener('click', function () {
+            this.classList.toggle('overlay')
+        });
+    }
+}else if (eleDifficulty == 'crazy') {
+    for (let i = 1; i <= 49; i++) {
+        const eleSq = document.createElement('div');
+        eleSq.classList.add('sq', 'crazy');
+        eleGrid.append(eleSq);
+        eleSq.append([i]);
+        
+        
+        eleSq.addEventListener('click', function () {
+            this.classList.toggle('overlay')
+        });
+    }
 }
-
+});
